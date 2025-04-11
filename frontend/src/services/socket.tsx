0dcc98216201ab3ddd1bb484dd3eebb9b82c8d6d
James Prentice <jamesprenticez@gmail.com>
2025-04-12 08:40:02 +1200

    room flow working - just needed the right backend url...

--- frontend/src/services/socket.tsx
+++ frontend/src/services/socket.tsx
@@ -2,7 +2,7 @@ import { io, Socket } from 'socket.io-client';
 import type { User, Room } from '../models';
 
 // Create a socket instance
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3001';
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000'
 const socket: Socket = io(SOCKET_URL);
 
 // Socket connection functions
