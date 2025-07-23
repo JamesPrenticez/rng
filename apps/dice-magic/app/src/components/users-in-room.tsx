d5aed35912b153962af076b5a5f1b1e656ee02d6
James Prentice <jamesprenticez@gmail.com>
2025-07-24 11:59:24 +1200

    Style the user seat display

--- apps/dice-magic/app/src/components/users-in-room.tsx
+++ apps/dice-magic/app/src/components/users-in-room.tsx
@@ -1,23 +1,68 @@
 import { useGameContext } from '@dice-magic/contexts';
 import { PlayerData } from '@dice-magic/models';
 import styled from '@emotion/styled';
-import { Button } from '@shared/components';
+import { Button, ButtonVariants } from '@shared/components';
 import { useBaseAppContext } from '@shared/contexts';
 import { useUserStore } from '@shared/stores';
 import { useMemo } from 'react';
+import { UserRoundPlus, UserRoundMinus, UserLock } from 'lucide-react';
+import clsx from 'clsx';
 
 const Container = styled.div`
   position: fixed;
   top: 0;
-  left: 0;
+  right: 0;
   padding: 1rem;
 
   color: var(--color-text);
+  font-family: "Quicksand";
   font-size: 2.4rem;
+  font-weight: 400;
   display: flex;
   flex-direction: column;
-
+  width: 35rem;
   z-index: 9999;
+
+  .user-title {
+    text-align: center;
+    color: var(--color-accent);
+    font-weight: 700;
+    overflow: hidden;
+  text-overflow: ellipsis;
+  }
+`;
+
+const SeatContainer = styled.div`
+  display: flex;
+  
+  p {
+    color: var(--color-grey-40);
+      overflow: hidden;
+  text-overflow: ellipsis;
+  }
+
+  .plus {
+    color: rgba(var(--color-rag-green-opacity), 0.8);
+  }
+
+  .minus {
+    color: rgba(var(--color-rag-red-opacity), 0.8);
+  }
+
+  .lock {
+    color: rgba(var(--color-rag-amber-opacity), 0.8);
+    cursor: default;
+  }
+`;
+
+const SeatButton = styled(Button)`
+  && {
+    margin-left: auto;
+
+    svg {
+      width: 2.8rem;
+    }
+  }
 `;
 
 export const UsersInRoom = () => {
@@ -41,25 +86,38 @@ export const UsersInRoom = () => {
 
   return (
     <Container>
-      <p>Current User: {user?.username}</p>
-
-      <p>Users: {baseAppState.users.length}</p>
+      <p className='user-title'>{user?.username}</p>
 
       {Array.from({ length: totalSeats }).map((_, seat) => {
         const player = seatToPlayer.get(seat);
         const isCurrentPlayer = player?.name === user?.username;
 
         return (
-          <p key={seat}>
-            Seat {seat + 1}: {player?.name ?? 'Empty'}
-            {(!player || isCurrentPlayer) && (
-              <Button onClick={() => handleSitOrLeave(seat, !!player)}>
-                {isCurrentPlayer ? 'leave' : 'sit'}
-              </Button>
+          <SeatContainer
+            key={seat}
+            className={clsx()}
+          >
+            <p>
+              {/* {seat + 1}: {player?.name ?? 'Empty'} */}{player?.name ?? 'Empty'}
+            </p>
+            {(!player || isCurrentPlayer) ? (
+              <SeatButton
+                variant={ButtonVariants.SKELETON}
+                onClick={() => handleSitOrLeave(seat, !!player)}
+              >
+                {isCurrentPlayer ? <UserRoundMinus className='minus' /> : <UserRoundPlus className='plus'/>}
+              </SeatButton>
+            ) : (
+               <SeatButton          variant={ButtonVariants.SKELETON}>
+              <UserLock className='lock'/>
+               </SeatButton>
             )}
-          </p>
+          </SeatContainer>
         );
-      })}
+        
+      })
+      }
+
     </Container>
   );
 };
--- libs/shared/ui/components/src/lib/button.tsx
+++ libs/shared/ui/components/src/lib/button.tsx
@@ -114,11 +114,12 @@ export const Button = ({
   children,
   onClick,
   isMobile,
+  className,
   ...rest
 }: ButtonProps) => {
   return (
     <ButtonContainer
-      className={clsx(variant, { mobile: isMobile })}
+      className={clsx(variant, { mobile: isMobile }, className)}
       onClick={onClick}
       {...rest}
     >
